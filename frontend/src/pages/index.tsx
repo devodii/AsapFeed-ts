361a2186d1c6ae8ae41b246c22c7d4e9e3bc1429
Emmanuel odii <emmanuelodii80@gmail.com>
2023-09-12 14:04:12 +0100

    test google ads

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,20 @@
 import { Hero } from "@/Components";
-import { Input } from "@/components/form/input";
-import { Label } from "@/components/form/label";
-import { Card } from "@/components/ui/card";
-import { useRef } from "react";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main
       className={`text-gray-900 flex flex-col min-h-screen container gap-4 md:gap-8 lg:gap-12 mx-auto px-4 md:px-12 lg:px-24 py-6`}
     >
+      <Head>
+        <title>AsapFeed</title>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2642908073199820"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <Hero />
-      {
-        // eslint-disable-next-line react/no-children-prop
-      }
-     
     </main>
   );
 }
